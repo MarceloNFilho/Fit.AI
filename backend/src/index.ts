481aa33e94570f2cb3226b5c9e83e4745b202d0a
marcelonfilho <marcelonfilho.ms@gmail.com>
2026-03-07 03:35:55 -0400

    fix: Add host 0.0.0.0 to server listen options.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import { env } from "./infrastructure/env.js";
 const app = await createServer();
 
 try {
-  await app.listen({ port: Number(env.PORT) });
+  await app.listen({ host: "0.0.0.0", port: Number(env.PORT) });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
